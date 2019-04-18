db49f1bd4d9a28be4afe0ef211eeee5939c45b19
Ivan <ivan.yonchovski@tick42.com>
2019-04-18 10:18:34 +0300

    WIP

---
+++ private/local/pidgin.el
@@ -0,0 +1,35 @@
+(require 'cl)
+(require 'pidgin-chatbuffer)
+(require 'pidgin-dbus)
+(require 'pidgin-chatactivity)
+
+(defconst pidgin-protocol-delimeter "|")
+
+(defvar pidgin-default-input-method default-input-method)
+
+(defvar pidgin-completing-read 'completing-read)
+
+(defvar pidgin-messenger-directory "~/.messenger/")
+
+(defun pidgin-connect ()
+  (pidgin-init))
+
+(defun pidgin-replace-regexp (regexp to-string)
+  (while (re-search-forward regexp nil t)
+    (replace-match to-string nil nil)))
+
+(defun pidgin-string-befor-p (prefix str)
+  (string-match (concat "^" prefix ".*") str))
+
+(defun pidgin-string-after-p (postfix str)
+  (string-match (concat postfix "$") str))
+
+(defun pidgin-delete-if (fn list)
+  (let (res)
+    (mapc (lambda (elem)
+            (unless (funcall fn elem)
+              (setq res (cons elem res))))
+          list)
+    (nreverse res)))
+
+(provide 'pidgin)
---
+++ private/local/pidgin/pidgin-dbus.el
@@ -0,0 +1,127 @@
+(require 'dbus)
+(require 'xml)
+
+;;http://habahaba.jrudevels.org/
+
+(defconst pidgin-icq-protocol "icq")
+
+(defconst pidgin-jabber-protocol "xmpp")
+
+(defvar pidgin-accounts nil)
+
+(defvar pidgin-all-user-list nil)
+
+(defvar pidgin-regexp-filter
+  '(("<br>\\|<br/>" "\n")
+    ("<a href='.*'>\\(.*\\)</a>" "\\1")))
+
+(defun pidgin-recieve-signal (account sender text conversation flags)
+  (let* ((protocol (car (rassoc account pidgin-accounts)))
+         (message (pidgin-parse-message text))
+         (sender-name (car (rassoc (list (car (split-string sender "/")))
+                                   (pidgin-user-list protocol)))))
+    (pidgin-chat-recieve
+     (pidgin-protocol-user-name sender-name)
+     (pidgin-protocol-user-name sender-name protocol)
+     (if (s-equals? message "<img id=\"2\">") "*thumb up*" (xml-substitute-special message)))))
+
+(defun pidgin-receive-my-signal (& args)
+  (message (list-length args)))
+
+
+(defun pidgin-parse-message (message)
+  (message "%s" (format "from pidgin: '%s'" message))
+  (with-temp-buffer
+    (insert message)
+    (mapc (lambda (regexp-info)
+            (goto-char (point-min))
+            (apply 'pidgin-replace-regexp regexp-info))
+          pidgin-regexp-filter)
+    (sgml-mode)
+    (sgml-tags-invisible 0)
+    (buffer-string)))
+
+(defun pidgin-init ()
+  (ignore-errors
+    (dbus-register-signal :session "im.pidgin.purple.PurpleService"
+                          "/im/pidgin/purple/PurpleObject"
+                          "im.pidgin.purple.PurpleInterface"
+                          "ReceivedImMsg"
+                          'pidgin-recieve-signal))
+  (setq pidgin-accounts (pidgin-account-list))
+  (setq pidgin-all-user-list
+        (mapcar (lambda (account-info)
+                  (list (car account-info)
+                        (pidgin-buddy-list (cdr account-info))))
+                pidgin-accounts)))
+
+;; (dbus-register-signal :session "im.pidgin.purple.PurpleService"
+;;                       "/im/pidgin/purple/PurpleObject"
+;;                       "im.pidgin.purple.PurpleInterface"
+;;                       "SentImMsg"
+;;                       'pidgin-receive-my-signal)
+;; (dbus-register-signal :session "im.pidgin.purple.PurpleService"
+;;                       "/im/pidgin/purple/PurpleObject"
+;;                       "im.pidgin.purple.PurpleInterface"
+;;                       "WroteChatMsg"
+;;                       'pidgin-receive-my-signal)
+
+
+(defun pidgin-send-message (to message)
+  (let* ((sender (split-string to pidgin-protocol-delimeter))
+         (name (car sender))
+         (protocol (second sender)))
+    (pidgin-dbus-send-message
+     (cdr (assoc protocol pidgin-accounts))
+     (second (assoc name (pidgin-user-list protocol)))
+     message)))
+
+(defmacro pidgin-dbus-purple-call-method (method &rest args)
+  `(dbus-call-method :session "im.pidgin.purple.PurpleService"
+                     "/im/pidgin/purple/PurpleObject"
+                     "im.pidgin.purple.PurpleInterface"
+                     ,method ,@args))
+
+(defun pidgin-account-list ()
+  (mapcar (lambda (account)
+            (cons (downcase
+                   (pidgin-dbus-purple-call-method
+                    "PurpleAccountGetProtocolName"
+                    :int32 account))
+                  account))
+          (pidgin-dbus-purple-call-method "PurpleAccountsGetAllActive")))
+
+
+
+(defun pidgin-dbus-send-message (account recipient message)
+  (let* ((conversation (pidgin-dbus-purple-call-method
+                        "PurpleConversationNew"
+                        :int32 1 :int32 account recipient))
+         (im (pidgin-dbus-purple-call-method
+              "PurpleConvIm"
+              :int32 conversation)))
+    (pidgin-dbus-purple-call-method
+     "PurpleConvImSend"
+     :int32 im (string-as-unibyte message))))
+
+
+(defun pidgin-user-list (protocol)
+  (second (assoc protocol pidgin-all-user-list)))
+
+(defun pidgin-buddy-list (account)
+  (mapcar (lambda (buddy)
+            (list (pidgin-dbus-purple-call-method "PurpleBuddyGetAlias" :int32 buddy)
+                  (pidgin-dbus-purple-call-method "PurpleBuddyGetName"  :int32 buddy)))
+          (pidgin-dbus-purple-call-method "PurpleFindBuddies" :int32 account "")))
+
+;; (mapcar (lambda (buddy)
+;;           (list (pidgin-dbus-purple-call-method "PurpleBuddyGetAlias" :int32 buddy)
+;;                 (pidgin-dbus-purple-call-method "PurpleBuddyGetName"  :int32 buddy)))
+;;         )
+;; (seq-map (lambda (id) (pidgin-dbus-purple-call-method "PurpleBuddyGetIcon" :int32 id))
+;;          (pidgin-dbus-purple-call-method "PurpleFindBuddies" :int32  2343 ""))
+
+;; (pidgin-dbus-purple-call-method "PurpleConvIm"
+;;                                 :int32 (nth 5 (pidgin-dbus-purple-call-method "PurpleGetIms")))
+
+(provide 'pidgin-dbus)
